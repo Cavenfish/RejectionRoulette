c2d2633fc7dece3838bcf51c89c15a74ec1e1093
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2026-04-13 18:58:25 +0200

    cleanup

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,5 +1,5 @@
 mod add_application;
-pub use add_application::{EntryForm, EntryFormProps};
+pub use add_application::EntryForm;
 
 mod tables;
 pub use tables::{ApplicationsTable, InterviewsTable};
